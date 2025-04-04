53a25fb6593faa4afcc8bf4f64caae575000d806
shinchana1011 <shinchanar.ai23@bmsce.ac.in>
2025-04-04 15:45:53 +0530

    Update storing_matrix.py

--- storing_matrix.py
+++ storing_matrix.py
@@ -2,11 +2,11 @@ from sympy import *
 import numpy as np
 from sympy import symbols,sin,cos
 x0,x1,x2,x3=symbols('x0 x1 x2 x3')
-f=sp.Matrix([[sin(x0+2*x1),2*x1+x3],[2*x0+x2,cos(2*x2+x3)]])
-k=sp.Matrix([[x0,x1],[x2,x3]])
+f=Matrix([[sin(x0+2*x1),2*x1+x3,2*x0+x2,cos(2*x2+x3)]])
+k=Matrix([[x0,x1,x2,x3]])
 display("F Matrix:",f)
 display("K Matrix:",k)
-reshape_f=f.reshape(4,1).jacobian(k.reshape(4,1))
-J_matrix=ImmutableDenseNDimArray(reshape_f).reshape(2,2,2,2)
+J_flat=f.jacobian(k)
+J= ImmutableDenseNDimArray(J_flat).reshape(2,2,2,2)
 print("the gradient of matrix is:")
-display(J_matrix)
+display(J)
